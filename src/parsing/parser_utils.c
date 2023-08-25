879a5313397ad9ce01b091af574901f7caf64571
SabaLeonel <leonel.saba@hotmail.com>
2023-08-25 15:19:17 +0200

    modified makefile for 42school

--- src/parsing/parser_utils.c
+++ src/parsing/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: lsaba-qu <leonel.sabaquezada@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 10:51:54 by lsaba-qu          #+#    #+#             */
-/*   Updated: 2023/08/25 11:30:06 by lsaba-qu         ###   ########.fr       */
+/*   Updated: 2023/08/25 14:14:02 by lsaba-qu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,33 +27,28 @@ char	*trim_matching_quotes(char *str, char quote)
 	return trimmed;
 }
 
-
-#include <stdio.h>
-#include <string.h>
-
-void trim_first_quote(char *command) {
-    char *start = command;
-
-    // Find the first quote or double quote
-    while (*start && *start != '\'' && *start != '\"') {
-        start++;
-    }
-
-    // If a quote is found, adjust the string
-    if (*start == '\'' || *start == '\"') {
-        char *end = start;
-
-        // Find the matching quote or double quote
-        while (*end && *end != *start) {
-            end++;
-        }
-
-        // If a matching quote or double quote is found, adjust the string
-        if (*end == *start) {
-            while (*end) {
-                *end = *(end + 1);
-                end++;
-            }
-        }
-    }
+void	trim_first_quote(char *command)
+{
+	char *start = command;	
+	// Find the first quote or double quote
+	while (*start && *start != '\'' && *start != '\"') {
+	    start++;
+	}	
+	// If a quote is found, adjust the string
+	if (*start == '\'' || *start == '\"') {
+		char *end = start;	
+		// Find the matching quote or double quote
+		while (*end && *end != *start)
+		{
+		    end++;
+		}
+		// If a matching quote or double quote is found, adjust the string
+		if (*end == *start) {
+			while (*end)
+			{
+				*end = *(end + 1);
+				end++;
+			}
+		}
+	}
 }
